0a0784de00fbab6341c16e45ad0f55b722230a06
Darryl Staflund <darryl.staflund@hushmail.com>
2016-05-01 10:09:38 -0600

    Add methods to child activity classes that will be used to start the activities.

--- app/src/main/java/com/github/dstaflund/geomemorial/activity/AboutActivity.java
+++ app/src/main/java/com/github/dstaflund/geomemorial/activity/AboutActivity.java
@@ -1,15 +1,25 @@
 package com.github.dstaflund.geomemorial.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.github.dstaflund.geomemorial.R;
 
 public class AboutActivity extends AppCompatActivity {
+    private static final String sLogTag = AboutActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public static void startActivity(final Context context){
+        Log.d(sLogTag, String.format("Starting %s", sLogTag));
+        final Intent intent = new Intent(context, AboutActivity.class);
+        context.startActivity(intent);
     }
 }
--- app/src/main/java/com/github/dstaflund/geomemorial/activity/FavoritesActivity.java
+++ app/src/main/java/com/github/dstaflund/geomemorial/activity/FavoritesActivity.java
@@ -1,15 +1,25 @@
 package com.github.dstaflund.geomemorial.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.github.dstaflund.geomemorial.R;
 
 public class FavoritesActivity extends AppCompatActivity {
+    private static final String sLogTag = FavoritesActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+    }
+
+    public static void startActivity(final Context context){
+        Log.d(sLogTag, String.format("Starting %s", sLogTag));
+        final Intent intent = new Intent(context, FavoritesActivity.class);
+        context.startActivity(intent);
     }
 }
